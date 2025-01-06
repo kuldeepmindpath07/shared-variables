c865db0aec8981088cb6c04bfccccf409d3b4d65
kuldeepmindpath07 <kuldeep.chakradhari@mindpathtech.com>
2025-01-06 11:55:40 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,5 +1,5 @@
 def call(String url, String branch){
   echo "this is clonning the code"
-  git url: url, branch: branch
+  git url: "${url}", branch: "${branch}"
   echo "clone done successfully"
 }
