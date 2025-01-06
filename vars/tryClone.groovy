025899b90c9566f3150dfe4638790173e6658aa3
kuldeepmindpath07 <kuldeep.chakradhari@mindpathtech.com>
2025-01-06 13:00:32 +0530

    Update tryClone.groovy

--- vars/tryClone.groovy
+++ vars/tryClone.groovy
@@ -1,3 +1,5 @@
-def tryClone(String url, String branch){
+def call(String url, String branch){
+  echo "this is clonning the code"
   git url: "${url}", branch: "${branch}"
+  echo "clone done successfully"
 }
